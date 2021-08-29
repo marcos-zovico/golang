e57035af5fb5a9c72e98c03bb02fedf0bb76a442
marcosouza <marcos.souza@mercadolivre.com>
2021-08-29 19:39:24 -0300

    Pacotes externo

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escvever()
+
+	erro := checkmail.ValidateFormat("123")
+	fmt.Println(erro)
 }
