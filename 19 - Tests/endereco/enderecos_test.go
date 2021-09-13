3ffd3319ad3fb587ab923850ed460b5cfdd85b52
marcosouza <marcos.souza@mercadolivre.com>
2021-09-13 09:50:31 -0300

    tests, coverage and tips

---
+++ 19 - Tests/endereco/enderecos_test.go
@@ -0,0 +1,44 @@
+package endereco
+
+import "testing"
+
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado  string
+}
+
+// go test --coverprofile cobertura.txt
+// go tool cover --func=cobertura.txt
+// go tool cover --html=cobertura.txt
+func TestTipoDeEndereco(t *testing.T) {
+	t.Parallel()
+
+	cenariosDeTeste := []cenarioDeTeste{
+		{"Rua ABC", "Rua"},
+		{"Avenida Paulista", "Avenida"},
+		{"Rodovia dos Imigrantes", "Rodovia"},
+		{"Praça das Rosas", "Tipo Inválido"},
+		{"Estrada Qualquer", "Estrada"},
+		{"RUA DOS BOBOS", "Rua"},
+		{"AVENIDA REBOUÇAS", "Avenida"},
+		{"", "Tipo Inválido"},
+	}
+
+	for _, cenario := range cenariosDeTeste {
+		retornoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+		if retornoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s",
+				retornoRecebido,
+				cenario.retornoEsperado,
+			)
+		}
+	}
+}
+
+func TestQualquer(t *testing.T) {
+	t.Parallel()
+
+	if 1 > 2 {
+		t.Errorf("Teste quebrou!")
+	}
+}
